de124ff899ecc0af4a944f74bcfdc688bfea82a2
xiusai <fasai_mist@hotmail.com>
2017-05-23 02:50:03 +0700

    lollol

---
+++ template/footer.php
@@ -0,0 +1,6 @@
+<script type="text/javascript" src="assets/js/jquery-3.1.1.min.js"></script>
+<script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="assets/js/sweetalert.min.js"></script>
+
+<script src="//cdnjs.cloudflare.com/ajax/libs/minicart/3.0.6/minicart.min.js"></script>
+<script type="text/javascript" src="assets/js/script.js"></script>
---
+++ template/header.php
@@ -0,0 +1,4 @@
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+<link rel="stylesheet" type="text/css" href="assets/css/sweetalert.css">
+<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="assets/css/bootstrap-yeti.min.css">
