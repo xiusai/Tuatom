a0ef1c3706be2621b13666394fb4b162944b80c3
xiusai <fasai_mist@hotmail.com>
2017-05-25 22:19:17 +0700

    Update forgot.php

--- forgot.php
+++ forgot.php
@@ -1,15 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	
 	<title>FORGOT PASSWORD</title>
 	<?php include "template/header.php" ?>
 	<style type="text/css">
 		section {
 			margin-top: 10%;
 		}
-
-
 		.box {
 
                 font-family: 'Lato', sans-serif;
@@ -18,10 +15,8 @@
     border: 10px solid  #EBF1F6 ;
 }
     .content{
-
         text-align: left;
     }
-
 	</style>
 </head>
 <body background="assets/images/wall2.jpg">
@@ -35,18 +30,12 @@
 					<section class="box">
 					<label class="control-label">Your Email </label>
 					<input type="email" class="form-control" name="Email" id= "Email" placeholder="Enter your Email here...." required>
-			
 				    </section>
 				</div>
-
 				<div class="form-group">
 					<button class="btn btn-primary" type="submit">Send</button>
-					
 				</div>
-
-			
 			</div>
-		
 		</form>
 	</section>
 </body>
