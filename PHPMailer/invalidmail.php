2dbba0727f9f194c3e3ac37b901a2ecd3e989966
xiusai <fasai_mist@hotmail.com>
2017-05-25 22:16:04 +0700

    Finish up path

--- PHPMailer/invalidmail.php
+++ PHPMailer/invalidmail.php
@@ -1,7 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	
 	<title>INVALID EMAIL</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
     <link rel="stylesheet" type="text/css" href="assets/css/sweetalert.css">
@@ -12,7 +11,6 @@
 		section {
 			margin-top: 10%;
 		}
-
 		.box {
 
                 font-family: 'Lato', sans-serif;
@@ -22,20 +20,16 @@
     border: 10px solid  #EBF1F6 ;
 }
     .content{
-
         text-align: left;
     }
-
 	</style>
 </head>
 <body background="wall2.jpg">
 	<header></header>
-	
 	<section class="box">
-		<!--<form id="form_login" action="POST"> -->
 		<h1>Sorry, you don't have an account yet.</h1><p>
-		<a href="http://127.0.0.1/projectse/regisform.php">Click here to Create Account</a><p>or<p>
-		<a href="http://127.0.0.1/projectse/login.php">Click here to Login</a>
+		<a href="http://127.0.0.1/Tuatom/regisform.php">Click here to Create Account</a><p>or<p>
+		<a href="http://127.0.0.1/Tuatom/login.php">Click here to Login</a>
 		
 	</section>
 </body>
